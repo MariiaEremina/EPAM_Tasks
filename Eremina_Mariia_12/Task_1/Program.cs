fa25a593de8020e2660eaf1b1149ec4b8f039344
MariiaEremina <mariiaeremina@gmail.com>
2018-03-23 09:06:55 +0300

    12

---
+++ Eremina_Mariia_12/Task_1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\User\Desktop\DotNetCourse\DotNetCourse\12 - Работа с файловой системой");
+
+            string filePath = Path.Combine(dir.FullName, "disposable_task_file.txt");
+
+            FileInfo file = new FileInfo(filePath);
+            string fileContent;
+
+            //using (StreamReader sr = file.OpenText())
+            //{
+            //    fileContent = sr.ReadToEnd();
+
+            //}
+
+            //string[] stringArr = MakeArr(fileContent);
+            //int[] squareArr = Square(stringArr);
+
+            //using (StreamWriter sw = file.AppendText())
+            //{
+            //    foreach (int num in squareArr)
+            //    {
+            //        sw.WriteLine(num);
+            //    }
+
+            //}
+
+
+            using (FileStream fs = file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+            {
+
+                StreamReader sr = new StreamReader(fs);
+                
+                    fileContent = sr.ReadToEnd();
+
+                
+
+                string[] stringArr = MakeArr(fileContent);
+                int[] squareArr = Square(stringArr);
+
+                fs.Seek(0, SeekOrigin.Begin);
+                StreamWriter sw = new StreamWriter(fs);
+
+                foreach (int num in squareArr)
+                    {
+                        sw.WriteLine(num);
+                    }
+                sw.Close();
+
+                
+            }
+        }
+
+        static string[] MakeArr(string sr)
+        {
+            string pattern = @"(\D)";
+            string replacement = " ";
+            string result = Regex.Replace(sr, pattern, replacement);
+            string[] arr = sr.Split(null as char[], StringSplitOptions.RemoveEmptyEntries);
+            return arr;
+        }
+
+        static int [] Square (string[] arr)
+        {
+            int[] sq = new int[(arr.Length)];
+            for (int i=0; i<arr.Length; i++)
+            {
+                int number = Int32.Parse(arr[i]);
+                sq[i] = number * number;
+            }
+            return sq;
+        }
+
+    }
+}
---
+++ Eremina_Mariia_12/Task_2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            DirectoryInfo dir = new DirectoryInfo(@"folder");
+            var i="0";
+            while (!(i == "1" || i == "2"))
+            {
+                Console.WriteLine("Нажмите 1, чтобы войти в режим наблюдения, 2, чтобы войти в режим отката изменений.");
+                i = Console.ReadLine();
+            }
+            if (i=="1")
+            {
+                Control();
+                string final;
+                do
+                {
+                    final = Console.ReadLine();
+                }
+                while (final != "x");
+            }
+            else
+            {
+                Console.WriteLine("Список созданных папок: ");
+                foreach (string d1 in Directory.GetDirectories(@"backup"))
+                {
+                    Console.WriteLine(d1.Substring(7));
+                }
+                Console.WriteLine("Скопируйте желаемые дату и время: ");
+                string date = Console.ReadLine();
+                Directory.Delete(@"folder", true); 
+                Directory.CreateDirectory(@"folder");
+                string datepath = $"backup\\{date}";
+                CopyDir(datepath, @"folder");
+                Console.ReadKey();
+            }
+           
+        }
+
+        public static void Control()
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = @"folder";
+            watcher.IncludeSubdirectories = true;
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            watcher.Filter = "*.txt";
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            //watcher.Created += new FileSystemEventHandler(OnChanged);
+            //watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            string name = DateTime.Now.ToString();
+            name = name.Replace(":", "-");
+            string path = "backup"+"\\" + name;
+            CopyDir(@"folder", path);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            string name = DateTime.Now.ToString();
+            name = name.Replace(":", "-");
+            string path = "backup" + "\\" + name;
+            CopyDir(@"folder", path);
+        }
+
+        //private static void OnRenamed(object source, RenamedEventArgs e)
+        //{
+        //    Thread.Sleep(5);
+        //    string name = DateTime.Now.ToString();
+        //    name = name.Replace(":", "-");
+        //    string path = "backup" + "\\" + name;
+        //    CopyDir(@"folder", path);
+        //}
+
+        static void CopyDir(string FromDir, string ToDir)
+        {
+            Directory.CreateDirectory(ToDir);
+            foreach (string s1 in Directory.GetFiles(FromDir))
+            {
+                string s2 = ToDir + "\\" + Path.GetFileName(s1);
+                File.Copy(s1, s2);
+            }
+            foreach (string s in Directory.GetDirectories(FromDir))
+            {
+                CopyDir(s, ToDir + "\\" + Path.GetFileName(s));
+            }
+        }
+    }
+}
