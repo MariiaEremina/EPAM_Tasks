027b31fe72fdede8d0ae8293e7409058978ce6a8
MariiaEremina <mariiaeremina@gmail.com>
2018-03-29 02:23:32 +0300

    fixed

--- Eremina_Mariia_Task08/Task1/DynamicArray.cs
+++ Eremina_Mariia_Task08/Task1/DynamicArray.cs
@@ -11,35 +11,37 @@ namespace Task1
     {
         private T[] arr;
         private T[] arr1;
+        private int length;
 
         public DynamicArray()
         {
             arr = new T[8];
-            for (int i = 0; i < 8; i++)
-            {
-                arr[i] = default(T);
-            }
+            length = 0;
         }
 
         public DynamicArray(int number)
         {
             if (number < 0)
             {
-                throw new ArgumentOutOfRangeException();
+                throw new ArgumentException("Количество ячеек меньше нуля");
             }
             else
             {
                 arr = new T[number];
-                for (int i = 0; i < number; i++)
-                {
-                    arr[i] = default(T);
-                }
+
             }
+            length = 0;
         }
 
-        public DynamicArray(T[] arr)
+        public DynamicArray(T[] getArr)
         {
-            this.arr = arr;
+            arr = new T[getArr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = getArr[i];
+            }
+            length = arr.Length;
         }
 
         public T this[int index]
@@ -52,7 +54,7 @@ namespace Task1
                 }
                 else
                 {
-                    throw new ArgumentOutOfRangeException();
+                    throw new ArgumentOutOfRangeException("Заданный индекс выходит за границу массива");
                 }
             }
         }
@@ -61,145 +63,186 @@ namespace Task1
         {
             if (Length == Capacity)
             {
-                int n = arr.Length;
-                arr1 = arr;
-                arr = new T[n * 2];
-                //for (int i = 0; i < arr1.Length; i++)
-                //{
-                //    arr[i] = arr1[i];
-                //}
-                Fill(arr, arr1, 0, n, 0,0);
-                arr[arr1.Length] = item;
+                //int n = arr.Length;
+                //arr1 = arr;
+                //arr = new T[n * 2];
+                ////for (int i = 0; i < arr1.Length; i++)
+                ////{
+                ////    arr[i] = arr1[i];
+                ////}
+                //Fill(arr, arr1, 0, n, 0,0);
+                ChangeCapacity(Capacity + 1);
+                arr[Length] = item;
             }
             else
             {
-                int i = 0;
-                while (!(arr[i].Equals(default(T))))
-                {
-                    i++;
-                }
-                arr[i] = item;
+                //int i = 0;
+                //while (!(arr[i].Equals(default(T))))
+                //{
+                //    i++;
+                //}
+                arr[length] = item;
             }
+            length++;
         }
 
-        public void AddRange(T[] item)
+        public void AddRange(T[] items)
         {
-            int l = item.Length;
-            int l1 = Length;
-
-            bool insert = false;
-            if ((Capacity - Length) >= l)
+            int newLength = Length + items.Length;
+            if (newLength <= Capacity)
             {
-                int i = 0;
-                int n = 0;
-                while (((n + i) < arr.Length - 1) && (n != l - 1))
-                {
-                    while (!(arr[i].Equals(default(T))))
-                    {
-                        i++;
-                    }
-                    while ((arr[i + n].Equals(default(T))) && ((n + i) < arr.Length) && (n != l - 1))
-                    {
-                        n++;
-                    }
-                }
-                if (n == l - 1)
-                {
-                    //for (int j = 0; j < l; j++)
-                    //{
-                    //    arr[j + i] = item[j];
-                    //}
-                    Fill(arr, item, 0, l, i, 0);
-                    insert = true;
-                }
+                items.CopyTo(arr, Length);
             }
-            if (!insert)
+            else
             {
-                int len = arr.Length;
-                while ((arr[len - 1].Equals(default(T))))
-                {
-                    len--;
-                }
-                arr1 = arr;
-                arr = new T[len + l];
-                //for (int j = 0; j < len; j++)
-                //{
-                //    arr[j] = arr1[j];
-                //}
-                Fill(arr, arr1, 0, len, 0, 0);
-                //for (int j = 0; j < item.Length; j++)
-                //{
-                //    arr[j + len] = item[j];
-                //}
-                Fill(arr, item, 0, item.Length, len, 0);
+                ChangeCapacity(newLength);
+                items.CopyTo(arr, Length);
             }
+            length = newLength;
+            //int l = item.Length;
+            //int l1 = Length;
 
+            //bool insert = false;
+            //if ((Capacity - Length) >= l)
+            //{
+            //    int i = 0;
+            //    int n = 0;
+            //    while (((n + i) < arr.Length - 1) && (n != l - 1))
+            //    {
+            //        while (!(arr[i].Equals(default(T))))
+            //        {
+            //            i++;
+            //        }
+            //        while ((arr[i + n].Equals(default(T))) && ((n + i) < arr.Length) && (n != l - 1))
+            //        {
+            //            n++;
+            //        }
+            //    }
+            //    if (n == l - 1)
+            //    {
+            //        //for (int j = 0; j < l; j++)
+            //        //{
+            //        //    arr[j + i] = item[j];
+            //        //}
+            //        Fill(arr, item, 0, l, i, 0);
+            //        insert = true;
+            //    }
+            //}
+            //if (!insert)
+            //{
+            //    int len = arr.Length;
+            //    while ((arr[len - 1].Equals(default(T))))
+            //    {
+            //        len--;
+            //    }
+            //    arr1 = arr;
+            //    arr = new T[len + l];
+            //    //for (int j = 0; j < len; j++)
+            //    //{
+            //    //    arr[j] = arr1[j];
+            //    //}
+            //    Fill(arr, arr1, 0, len, 0, 0);
+            //    //for (int j = 0; j < item.Length; j++)
+            //    //{
+            //    //    arr[j + len] = item[j];
+            //    //}
+            //    Fill(arr, item, 0, item.Length, len, 0);
+            //}
+            //length = length + item.Length;
         }
 
         public bool Remove(int index)
         {
-            if (index < arr.Length)
+
+            if (index < Length)
             {
-                //for (int i = index + 1; i < arr.Length; i++)
-                //{
-                //    arr[i - 1] = arr[i];
-                //}
-                Fill(arr, arr, index + 1, arr.Length, -1, 0);
-                arr[arr.Length - 1] = default(T);
+                ////for (int i = index + 1; i < arr.Length; i++)
+                ////{
+                ////    arr[i - 1] = arr[i];
+                ////}
+
+                //Fill(arr, arr, index + 1, arr.Length, -1, 0);
+                //arr[Length - 1] = default(T);
+
+                arr1 = new T[Length - index - 1];
+                arr1 = arr.Skip(index + 1).ToArray();
+                arr1.CopyTo(arr, index);
+                
+
+                length--;
                 return true;
             }
             else
             {
                 return false;
             }
+
         }
 
         public void Insert(int index, T something)
         {
-            if (index < arr.Length)
+            if (index < Length)
             {
-                int len = arr.Length-1;
-                while ((arr[len].Equals(default(T)))&& (len>0))
-                {
-                    len--;
-                }
-                if (len+1 == arr.Length)
-                {
-                    arr1 = arr;
-                    arr = new T[len + 2];
-                    //for (int i = 0; i < index; i++)
-                    //{
-                    //    arr[i] = arr1[i];
-                    //}
-                    Fill(arr, arr1, 0, index, 0, 0);
-                    //for (int i = len+1; i > index; i--)
-                    //{
-                    //    arr[i] = arr1[i - 1];
-                    //}
-                    ReverseFill(arr, arr1, len + 1, index, 0, -1);
 
-                }
-                else
+                arr1 = new T[Length - index];
+                arr1 = arr.Skip(index).ToArray();
+                if (Length == Capacity)
                 {
-                    //for (int i = len; i > index; i--)
-                    //{
-                    //    arr[i] = arr[i - 1];
-                    //}
-                    ReverseFill(arr, arr1, len, index, 0, -1);
+                    ChangeCapacity(Capacity + 1);
                 }
                 arr[index] = something;
+                //arr1.CopyTo(arr, index + 1);
+
+                Array.Copy(arr1, 0, arr, index + 1, Length - index);
+                length++;
+
+                //length++;
+                //int len = arr.Length-1;
+                //while ((arr[len].Equals(default(T)))&& (len>0))
+                //{
+                //    len--;
+                //}
+                //if (len+1 == arr.Length)
+                //{
+                //    arr1 = arr;
+                //    arr = new T[len + 2];
+                //    //for (int i = 0; i < index; i++)
+                //    //{
+                //    //    arr[i] = arr1[i];
+                //    //}
+                //    Fill(arr, arr1, 0, index, 0, 0);
+                //    //for (int i = len+1; i > index; i--)
+                //    //{
+                //    //    arr[i] = arr1[i - 1];
+                //    //}
+                //    ReverseFill(arr, arr1, len + 1, index, 0, -1);
+
+                //}
+                //else
+                //{
+                //    //for (int i = len; i > index; i--)
+                //    //{
+                //    //    arr[i] = arr[i - 1];
+                //    //}
+                //    ReverseFill(arr, arr1, len, index, 0, -1);
+                //}
+                //arr[index] = something;
+            }
+            else if (index == length)
+            {
+                Add(something);
             }
             else
             {
-                throw new ArgumentOutOfRangeException();
+                throw new ArgumentOutOfRangeException("Заданный индекс выходит за границу массива");
             }
         }
 
-        private void Fill (T[]arr, T[]arr1, int startPoint, int stopPoint, int koeff1, int koeff2)
+        private void Fill(T[] arr, T[] arr1, int startPoint, int stopPoint, int koeff1, int koeff2)
         {
             for (int i = startPoint; i < stopPoint; i++)
             {
-                arr[i+koeff1] = arr1[i+koeff2];
+                arr[i + koeff1] = arr1[i + koeff2];
             }
         }
 
@@ -224,26 +267,45 @@ namespace Task1
         {
             get
             {
-                int length = 0;
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    if (!(arr[i].Equals(default(T))))
-                    {
-                        length++;
-                    }
-                }
+                //int length = 0;
+                //for (int i = 0; i < arr.Length; i++)
+                //{
+                //    if (!(arr[i].Equals(default(T))))
+                //    {
+                //        length++;
+                //    }
+                //}
                 return length;
             }
         }
 
-        public T[] Arr
+        //public T[] Arr
+        //{
+        //    get
+        //    {
+        //        return arr;
+        //    }
+        //}
+
+        private void ChangeCapacity(int newCapacity)
         {
-            get
+            var tmp = new T[newCapacity];
+            this.arr.CopyTo(tmp, 0);
+            this.arr = tmp;
+        }
+
+        public string ForPrint()
+        {
+            string forPrint = "";
+            for (int i = 0; i < Length; i++)
             {
-                return arr;
+                forPrint += arr[i].ToString() + " ";
             }
+            return forPrint;
         }
     }
+
+
 }
 
 
--- Eremina_Mariia_Task08/Task1/Program.cs
+++ Eremina_Mariia_Task08/Task1/Program.cs
@@ -10,7 +10,26 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-
+            int[] numberArr = new int[] {1,2,3,4,5,6,7,8,9 };
+            DynamicArray<int> testArr = new DynamicArray<int>(numberArr);
+            Console.WriteLine(testArr.ForPrint());
+            testArr.Add(15);
+            Console.WriteLine(testArr.ForPrint());
+            //testArr.AddRange(numberArr);
+            //Console.WriteLine(testArr.ForPrint());
+            testArr.Remove(0);
+            Console.WriteLine(testArr.ForPrint());
+            testArr.Remove(6);
+            Console.WriteLine(testArr.ForPrint());
+            testArr.Remove(testArr.Length - 1);
+            Console.WriteLine(testArr.ForPrint());
+            testArr.Insert(0, 88);
+            Console.WriteLine(testArr.ForPrint());
+            testArr.Insert(6, 88);
+            Console.WriteLine(testArr.ForPrint());
+            testArr.Insert(testArr.Length, 88);
+            Console.WriteLine(testArr.ForPrint());
+            Console.ReadKey();
         }
     }
 }
